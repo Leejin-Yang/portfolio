efc902297bff606c0ae9d0cbfbf4a75530aac408
Leejin-Yang <ejin1996@gmail.com>
2022-12-09 23:32:50 +0900

    style: 함수 타입 스타일 수정

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,4 +1,4 @@
-import type { MouseEvent } from 'react';
+import type { MouseEventHandler } from 'react';
 import { useRecoilState } from 'recoil';
 import store from 'store';
 
@@ -8,7 +8,7 @@ const useTheme = () => {
   const [, setBackgroundTheme] = useRecoilState(backgroundThemeMode);
   const [, setColorTheme] = useRecoilState(colorThemeMode);
 
-  const handleBackgroundTheme = (e: MouseEvent<HTMLButtonElement>) => {
+  const handleBackgroundTheme: MouseEventHandler<HTMLButtonElement> = (e) => {
     const newTheme = e.currentTarget.dataset.theme || 'light';
 
     setBackgroundTheme(newTheme);
@@ -16,7 +16,7 @@ const useTheme = () => {
     store.set('leejin.background.theme', newTheme);
   };
 
-  const handleColorTheme = (e: MouseEvent<HTMLButtonElement>) => {
+  const handleColorTheme: MouseEventHandler<HTMLButtonElement> = (e) => {
     const newTheme = e.currentTarget.dataset.theme || 'orange';
 
     setColorTheme(newTheme);
